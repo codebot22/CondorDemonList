5e798c0f45a9d77181d3d42b44ba5c7e6b9045a3
JodyCondor <jody.tigrou@gmail.com>
2025-04-15 20:25:40 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,7 +100,8 @@ export default {
                     </template>
                     <h3>Welcome to the Condor Demonlist !</h3>
                     <p>
-                        TO SUBMIT A RECORD : Click on the "Submit Record" button to go on my Discord server and send your clip WITH CLICKS (and cheat indicator if you can) to get it moderated
+                        TO SUBMIT A RECORD :
+Click on the "Submit Record" button to go on my Discord server and send your clip WITH CLICKS (and cheat indicator if you can) to get it moderated
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
